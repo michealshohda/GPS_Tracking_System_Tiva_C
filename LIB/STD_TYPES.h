df85b43e6d5b6822b7acb92c313f8c8f290b1a75
Fadyrefat <167587049+Fadyrefat@users.noreply.github.com>
2024-04-24 02:35:42 -0700

    Create STD_TYPES.h

---
+++ LIB/STD_TYPES.h
@@ -0,0 +1,12 @@
+#ifndef STD_TYPES_H_
+#define STD_TYPES_H_
+
+
+typedef unsigned char uint8_t;
+typedef signed char int8_t;
+typedef unsigned short uint16_t;
+typedef signed short int16_t;
+typedef unsigned long uint32_t;
+typedef signed long int32_t;
+
+#endif
