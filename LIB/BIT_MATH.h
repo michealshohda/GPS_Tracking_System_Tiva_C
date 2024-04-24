d1c3ccf7d104f521ea02a933f5ca88be2302edcc
YoustinaZak <125680172+YoustinaZak@users.noreply.github.com>
2024-04-24 17:29:22 +0200

    Update BIT_MATH.h

--- LIB/BIT_MATH.h
+++ LIB/BIT_MATH.h
@@ -1,7 +1,7 @@
 #ifndef BIT_MATH_H_
 #define BIT_MATH_H_
 
-#define SET_BIT(regist,bit)    (regis |=(1<<bit))
+#define SET_BIT(regis,bit)    (regis |=(1<<bit))
 #define CLEAR_BIT(regis,bit)   (regis &= ~(1<<bit))
 #define GET_BIT(regis,bit)     (regis &(1<<bit))
 #define TOGGLE_BIT(regis,bit)  (regis ^=(1<<bit))
