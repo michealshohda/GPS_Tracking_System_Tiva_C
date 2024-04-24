44e79a7cb93263717ab012896410834bdfbb953c
germinechawki <155088106+germinechawki@users.noreply.github.com>
2024-04-24 14:36:26 +0200

    Update BIT_MATH.h

--- LIB/BIT_MATH.h
+++ LIB/BIT_MATH.h
@@ -1,1 +1,10 @@
+#ifndef BIT_MATH_H_
+#define BIT_MATH_H_
 
+#define SET_BIT(regist,bit)    (regis |=(1<<bit))
+#define CLEAR_BIT(regis,bit)   (regis &= ~(1<<bit))
+#define GET_BIT(regis,bit)     (regis &(1<<bit))
+#define TOGGLE_BIT(regis,bit)  (regis ^=(1<<bit))
+
+
+#endif
