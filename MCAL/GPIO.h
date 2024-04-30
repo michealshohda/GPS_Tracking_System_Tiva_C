328e419af9cd69613eabb714b1a07a2f57cef596
michealshohda <112519102+michealshohda@users.noreply.github.com>
2024-04-30 13:10:27 +0300

    Update GPIO.h
    defining ports SysRC 
    defining ports initialization functions

--- MCAL/GPIO.h
+++ MCAL/GPIO.h
@@ -1,1 +1,21 @@
+#ifndef GPIO_H_
+#define GPIO_H_
 
+#include "../LIB/tm4c123gh6pm.h"
+#include "../LIB/BIT_MATH.h"
+
+#define PORTA_SysRC       0x01
+#define PORTB_SysRC       0x02
+#define PORTC_SysRC       0x04
+#define PORTD_SysRC       0x08
+#define PORTE_SysRC       0x10
+#define PORTF_SysRC       0x20
+
+void PortA_Init();
+void PortB_Init();
+void PortC_Init();
+void PortD_Init();
+void PortE_Init();
+void PortF_Init();
+
+#endif
